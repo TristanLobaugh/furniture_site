999ca5a252175c68847253d6d0cc7adf1ee4f5f8
TristanLobaugh <ttrader_atl@yahoo.com>
2016-05-25 16:28:55 -0400

    commit

--- wp-content/themes/twentysixteen/page-Gallery.php
+++ wp-content/themes/twentysixteen/page-Gallery.php
@@ -1,10 +1,18 @@
-<?php
-
-get_header(); ?>
+<?php get_header(); ?>
+<?php $term = $_GET['term']; 
+	if(!isset($term)){
+		$term = "all";
+	}
+?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<?php echo photo_gallery(1); ?>
+		<!-- <?php echo photo_gallery(1); ?> -->
+		<?php $red_images =  get_image_urls_by_taxonomy_term($term); 
+		foreach($red_images as $image){
+			print '<img src="'.$image.'">';
+		}
+		?>
 	</main>
 </div>
 <?php get_footer(); ?>
